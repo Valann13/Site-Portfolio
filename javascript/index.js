165e4f2d7b5f4a9867234946ce663ba53052b0cd
Valann13 <cdslaboite@hotmail.com>
2023-07-25 11:31:04 +0200

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,130 @@
+
+// Animation des progress bar page compétence
+
+
+
+let viewableOffset = $("#Comp").offset().top - $(window).scrollTop()
+
+window.onscroll = function() {Progress()}; 
+
+function Progress() {
+        if ($(this).scrollTop() > viewableOffset) {
+
+            
+$(".progress-bar1").animate({
+    width: "80%"
+}, 3000);
+
+$(".progress-bar2").animate({
+    width: "70%"
+}, 3000);
+
+$(".progress-bar3").animate({
+    width: "70%"
+}, 3000);
+
+$(".progress-bar4").animate({
+    width: "60%"
+}, 3000);
+
+$(".progress-bar5").animate({
+    width: "60%"
+}, 3000);
+
+$(".progress-bar6").animate({
+    width: "60%"
+}, 3000);
+
+$(".progress-bar7").animate({
+    width: "80%"
+}, 3000);
+
+$(".progress-bar8").animate({
+    width: "70%"
+}, 3000);
+
+$(".progress-bar9").animate({
+    width: "60%"
+}, 3000);
+
+$(".progress-bar10").animate({
+    width: "60%"
+}, 3000);
+
+
+
+$(".progress-bar11").animate({
+    
+    width: "80%"
+}, 3000);
+
+$(".progress-bar12").animate({
+    width: "90%"
+}, 3000);
+
+$(".progress-bar13").animate({
+    width: "75%"
+}, 3000);
+
+$(".progress-bar14").animate({
+    width: "70%"
+}, 3000);
+
+$(".progress-bar15").animate({
+    width: "85%"
+}, 3000);
+
+$(".progress-bar16").animate({
+    width: "90%"
+}, 3000);
+
+
+$(".progress-bar17").animate({
+    width: "90%"
+}, 3000);
+
+
+$(".progress-bar18").animate({
+    width: "90%"
+}, 3000);
+
+$(".progress-bar19").animate({
+    width: "90%"
+}, 3000);
+
+$(".progress-bar20").animate({
+    width: "70%"
+}, 3000);
+
+$(".progress-bar21").animate({
+    width: "90%"
+}, 3000);
+
+$(".progress-bar22").animate({
+    width: "65%"
+}, 3000);
+
+$(".progress-bar23").animate({
+    width: "75%"
+}, 3000);
+
+
+
+        }
+    }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
